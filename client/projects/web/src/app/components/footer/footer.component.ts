9b72db6f4bc2b03c97c0413f0ef41e74ca7c474d
Filip Luac <oliveripepa1207>
2021-07-02 10:45:22 +0200

    fix: updated rules

--- client/projects/web/src/app/components/footer/footer.component.ts
+++ client/projects/web/src/app/components/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'ltc-footer',
@@ -7,10 +7,10 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FooterComponent implements OnInit {
-
   constructor() { }
-  year = new Date().getFullYear();
-  ngOnInit(): void {
-  }
 
+  year = new Date().getFullYear();
+
+  ngOnInit() {
+  }
 }
