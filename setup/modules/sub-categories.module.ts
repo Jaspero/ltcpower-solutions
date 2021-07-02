1f37c6934e56c34c711df7913ee41fc043e7b225
floyyd6 <slobodantomic82@gmail.com>
2021-07-02 13:08:19 +0200

    chore(cms):adding sub-categories collection

---
+++ setup/modules/sub-categories.module.ts
@@ -0,0 +1,76 @@
+import {CREATED_ON} from './shared/created-on';
+
+export const SUBCATEGRIESS_MODULE = {
+  id: 'sub-categories',
+  name: 'Sub-categories',
+  description: 'Collection of sub-categories',
+  authorization: {
+    read: ['admin'],
+    write: ['admin']
+  },
+  layout: {
+    editTitleKey: 'name',
+    instance: {
+      segments: [
+        {
+          components: [
+            {
+              selector: 'duplicate'
+            }
+          ]
+        },
+        {
+          fields: [
+            '/title',
+            '/image'
+          ]
+        }
+      ]
+    },
+    table: {
+      tableColumns: [
+        {
+          key: '/title',
+          label: 'Title'
+        },
+        {
+          key: '/image',
+          label: 'Image'
+        }
+      ],
+      actions: [
+        {
+          value: `it => '<jms-e-new-prepopulate collection="users" data-name="Prefill Test" data-email="{{it.data.description}}" label="Assign User"></jms-e-new-prepopulate>'`
+        }
+      ]
+    }
+  },
+  schema: {
+    properties: {
+      title: {
+        type: 'string'
+      },
+      image: {
+        type: 'string'
+      },
+    },
+    required: [
+      'title',
+      'image'
+    ]
+  },
+  definitions: {
+    title: {
+      label: 'Title'
+    },
+    image: {
+      label: 'Image',
+      component: {
+        type: 'image'
+      }
+    },
+  },
+  metadata: {
+    autoSave: 0
+  }
+};
